67ebbfef7681e02dd235ff5f116fa50c40eea986
suhasx00td <m.suhashkumar@gmail.com>
2025-04-26 18:03:00 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,9 +37,8 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
  # Version & Variant
-export FOX_VERSION="R12.1"
+export FOX_MAINTAINER_PATCH_VERSION="01"
 export FOX_VARIANT="A15"
-export FOX_BUILD_TYPE="Stable"
 
  # Binaries & Tools
 export FOX_USE_BASH_SHELL=1
